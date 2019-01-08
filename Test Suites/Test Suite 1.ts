58bc7c37b28d6e17f8e4da38c9957b843c50f13d
awisniewska <awisniewska@DESKTOP-NUN04OB>
2019-01-08 13:55:08 +0100

    first commit

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-13T12:34:46</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>660d360b-3c13-488e-8c7b-963007b9c729</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5abf680-1d0c-445a-bb23-0be511ac8721</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d47a3e2c-7940-4493-8b46-7e6c38b3be43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08c5b5b9-fa13-40ab-88cb-0957033aa213</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LIstUSersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69ae1894-8fcb-4e5d-80c9-a9e8a032c87c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
